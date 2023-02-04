70a3975cb49b60df0bb7b3c6b171915f857fd031
Owen Winkler <ringmaster@midnightcircus.com>
2023-02-04 17:34:44 -0500

    Working login

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,106 @@
+<script>
+    import { pbStore, User } from 'svelte-pocketbase';
+    import { env } from '$env/dynamic/public';
+    
+    pbStore.set(env.PUBLIC_POCKETBASE_URL);
+
+    function doLogout() {
+        $pbStore.authStore.clear()
+        document.cookie = $pbStore.authStore.exportToCookie({ httpOnly: false, expires: 0 })
+        document.location = "/"
+    }
+</script>
+
+<svelte:head>
+<script src="https://kit.fontawesome.com/7e2abaa70e.js" crossorigin="anonymous"></script>
+</svelte:head>
+
+<div class="wrapper">
+    <nav class="navigation">
+        <div class="container">
+            <a href="/" class="navigation-title">
+                <i class="fa-sharp fa-solid fa-wave-pulse"></i>
+                <h1 class="title">Teambeat</h1>
+            </a>
+            <ul class="navigation-list float-right">
+                <li class="navigation-item">
+                    <User let:user>
+                        <a href="/account" class="navigation-link">
+                            <i class="fa-solid fa-user"></i>
+                            {user.name}
+                        </a>
+                        <a href="#" class="navigation-link" on:click="{doLogout}"><i class="fa-solid fa-right-from-bracket"></i></a>
+                        <span slot="signedout">
+                            <a href="/" class="navigation-link">
+                                <i class="fa-solid fa-user"></i>
+                                Log In
+                            </a>        
+                        </span>
+                    </User>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <slot/>
+    </div>
+</div>
+
+<style>
+    @import "../../node_modules/milligram/dist/milligram.min.css";
+    :global(html) {
+        color: red;
+    }
+    .wrapper {
+        display: block;
+        overflow: hidden;
+        position: relative;
+        width: 100%;
+    }
+    .navigation {
+        background: #f4f5f6;
+        border-bottom: 0.1rem solid #d1d1d1;
+        display: block;
+        height: 5.2rem;
+        left: 0;
+        max-width: 100%;
+        position: fixed;
+        right: 0;
+        top: 0;
+        width: 100%;
+        z-index: 2;
+    }
+    
+    .wrapper>.container {
+        padding-bottom: 7.5rem;
+        padding-top: 7.5rem;
+    }
+    .wrapper .container {
+        max-width: 80rem;
+    }
+    .navigation .navigation-link, .navigation .navigation-title, .navigation .title {
+        display: inline;
+        font-size: 1.6rem;
+        line-height: 5.2rem;
+        padding: 0;
+        text-decoration: none;
+    }
+    
+    .navigation .navigation-title, .navigation .title {
+        color: #606c76;
+        font-family: Gotham Rounded A,Gotham Rounded B,Helvetica Neue,Arial,sans-serif;
+        position: relative;
+    }
+    .navigation .navigation-list {
+        list-style: none;
+        margin-bottom: 0;
+        margin-right: 5rem;
+    }
+    
+    .navigation .navigation-item {
+        float: left;
+        margin-bottom: 0;
+        margin-left: 2.5rem;
+        position: relative;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
+<script>
+    import { pbStore, User } from 'svelte-pocketbase';
+    import { env } from '$env/dynamic/public';
+    
+    pbStore.set(env.PUBLIC_POCKETBASE_URL);
+    
+    let username = ''
+    let password = ''
+    
+    async function doLogin() {
+        const authData = await $pbStore.collection('users').authWithPassword(username, password)
+        document.cookie = $pbStore.authStore.exportToCookie({ httpOnly: false })
+        document.location = "/"
+    }
+</script>
+
 <h1>Welcome to Teambeat</h1>
 
+<p>Teambeat is a tool for realtime retrospectives, surveys, and team pulse checks.</p>
+
+<User let:user>
+    <div>
+        <p>Name: {user?.name}</p>
+        <p>Email: {user?.email}</p>
+    </div>
+    <div slot="signedout">
+        <form on:submit={doLogin}>
+            <fieldset>
+                <label for="username">Username</label>
+                <input type="text" placeholder="username" id="username" bind:value="{username}">
+                
+                <label for="password">Password</label>
+                <input type="password" placeholder="password" id="password" bind:value="{password}">
+                
+                <input class="button-primary" type="submit" value="Send">
+            </fieldset>
+        </form>
+    </div>
+</User>
