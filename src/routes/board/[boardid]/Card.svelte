8693143954a4d89d138c9942bb2568749851750b
Owen Winkler <ringmaster@midnightcircus.com>
2023-02-19 11:35:31 -0500

    Votes and starting scenes

--- src/routes/board/[boardid]/Card.svelte
+++ src/routes/board/[boardid]/Card.svelte
@@ -4,6 +4,12 @@
     const dispatch = createEventDispatcher();
     
     export let card;
+
+    let votes = [
+        {"title": "Vote", "count": 0}
+    ];
+
+    let editing = false;
     
     function handleDragStart(e) {
         dispatch("dragstart", e)
@@ -12,14 +18,28 @@
     function handleDragEnd(e) {
         dispatch("dragend", e)
     }
+
+    function handleEdit(e) {
+        editing = true;
+    }
+
+    function handleDoneEdit(e) {
+        editing = false;
+    }
     
 </script>
 
 
-<sl-card class="card" id={card.id} draggable=true on:dragstart={handleDragStart} on:dragend={handleDragEnd} >
-    {card.description}
+<sl-card class="card" id={card.id} draggable="{!editing}" on:dragstart={handleDragStart} on:dragend={handleDragEnd} >
+    <div contenteditable="{editing}" on:dblclick={handleEdit} on:blur={handleDoneEdit}>{card.description}</div>
     <div slot="footer">
-        <sl-rating></sl-rating>
+        <div>
+            {#each votes as vote}
+            <sl-tooltip content="{vote.title}">
+                {vote.count} <i class="fa-regular fa-ballot-check"></i>
+            </sl-tooltip>                
+            {/each}
+        </div>
         <div>
             <i class="fa-regular fa-face-smile"></i>
             <sl-tooltip content="{card.expand.user.name}">
@@ -34,6 +54,7 @@
         border-width: 3px 1px 1px;
         box-shadow: var(--sl-shadow-large);
         margin-bottom: 1rem;
+        width: 100%;
     }
     .card [slot='footer'] {
         display: flex;
