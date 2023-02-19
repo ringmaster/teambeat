8b08811e005fe9b744f579528d8eb05612c85518
Owen Winkler <ringmaster@midnightcircus.com>
2023-02-18 20:57:51 -0500

    Updating card columns

--- src/routes/board/[boardid]/+page.svelte
+++ src/routes/board/[boardid]/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { pbStore } from "svelte-pocketbase";
+    import Card from "./Card.svelte";
     
     export let data;
     
@@ -29,7 +30,6 @@
         return alert.toast();
     }
     
-    
     async function getBoard() {
         let boardData = await $pbStore.collection('boards').getOne(data.params.boardid, {expand: "users,facilitators,scenes(board),columns(board)"});
         let board = {
@@ -39,17 +39,23 @@
         let columnData = boardData.expand["columns(board)"];
         
         board.columns = columnData.map(async (column) => {
-            let cardData = await $pbStore.collection('cards').getFullList(10, {
-                sort: "+created",
-                filter: 'column = "' + column.id + '"',
-                expand: 'user',
-                '$cancelKey': column.id // Wihtout this, the client cancels separate column requests as non-unique
-            })
-            column.cards = cardData;
+            await column;
+            column.reload = ()=>column.cards = loadCards(column);
+            column.reload();
             return column;
         })
         
         return board;
+    }
+    
+    async function loadCards(column) {
+        let cardData = await $pbStore.collection('cards').getFullList(10, {
+            sort: "+created",
+            filter: 'column = "' + column.id + '"',
+            expand: 'user',
+            '$cancelKey': column.id // Wihtout this, the client cancels separate column requests as non-unique
+        })
+        return cardData;
     }
     
     let board = getBoard();
@@ -58,33 +64,35 @@
     // https://www.javascripttutorial.net/web-apis/javascript-drag-and-drop/
     
     function handleDragStart(e) {
-        status = "Dragging the element " + e
-        .currentTarget
-        .getAttribute('id');
+        status = "Dragging the element " + e.detail.currentTarget.getAttribute('id');
         // This sets the class on the original before the dragged clone is made
-        window.setTimeout(()=>e.target.classList.add('novis'), 0);
-        e.dataTransfer.dropEffect = "move";
-        e.dataTransfer
-        .setData("text", e.target.getAttribute('id'));
+        window.setTimeout(()=>e.detail.target.classList.add('novis'), 0);
+        e.detail.dataTransfer.dropEffect = "move";
+        e.detail.dataTransfer.setData("text", e.detail.target.getAttribute('id'));
     }
     
     function handleDragEnd(e) {
         e.preventDefault();
         if (dropped_in == false) {
-            e.target.classList.remove('novis');
-            status = "You let the " + e
-            .target
-            .getAttribute('id') + " go.";
+            window.setTimeout(()=>e.detail.target.classList.remove('novis'), 0);
+            console.log(e);
+            status = "You let the " + e.detail.target.getAttribute('id') + " go.";
         }
         dropped_in = false;
     }
     
     function handleDragEnter(e) {
         e.preventDefault();
-        status = "You are dragging over the " + e
-        .currentTarget
-        .getAttribute('id');
-        console.log(e)
+        status = "You are dragging over " + e.currentTarget.getAttribute('id');
+        let columnOver = document.getElementsByClassName('column-over')
+        for(let element of columnOver) {
+            element.classList.remove('column-over');
+        };
+        e.target.closest('.column').classList.add('column-over')
+    }
+    
+    function handleDragOver(e) {
+        e.preventDefault();
         let columnOver = document.getElementsByClassName('column-over')
         for(let element of columnOver) {
             element.classList.remove('column-over');
@@ -112,6 +120,8 @@
         status = "You droped " + element_id + " into drop zone " + e.target.closest('.column').id;
         notify(status)
         
+        console.log("BOARD", board);
+        
         // Load the initial card data
         let record = await $pbStore.collection('cards').getOne(element_id);
         
@@ -122,7 +132,24 @@
         record = await $pbStore.collection('cards').update(element_id, record);
         
         // Reload the whole board
-        board = getBoard();
+        //board = getBoard();
+        
+        // Reload the destination column
+        board.then((board)=>{
+            console.log("COLUMNS 2", board);
+            for(let column of board.columns) {
+                column.then((column) => {
+                    console.log(column.id, e.target.closest('.column').id)
+                    //if(column.id == e.target.closest('.column').id) {
+                        column.cards = loadCards(column);
+                    //}
+                    return column;
+                })
+            }
+            return board;
+        })
+        
+        // Reload the source column
     }
     
 </script>
@@ -145,43 +172,27 @@
             {#each board.columns as column}
             {#await column then column}
             
-            <div class="column"
-            on:dragenter={handleDragEnter} 
-            on:dragleave={handleDragLeave}  
-            on:dragover={handleDragEnter}
-            on:drop={handleDragDrop} 
-            id="{column.id}"
-            >
-            <h2>{column.title}</h2>
-            
-            {#await column.cards then cards}
-            {#if cards}
-            {#each cards as card(card.id)}
-            <sl-card class="card" id={card.id} draggable=true on:dragstart={handleDragStart} on:dragend={handleDragEnd}>
-                {card.description}
-                <div slot="footer">
-                    <sl-rating></sl-rating>
-                    <sl-tooltip content="{card.expand.user.name}">
-                        <i class="fa-solid fa-user"></i>
-                    </sl-tooltip>
+            <div class="column" on:dragenter={handleDragEnter} on:dragleave={handleDragLeave} on:dragover={handleDragOver} on:drop={handleDragDrop} id="{column.id}">
+                <h2>{column.title}</h2>
+                
+                {#await column.cards then cards}
+                {#each cards as card(card.id)}
+                <Card bind:card={card} on:dragstart={handleDragStart} on:dragend={handleDragEnd} />
+                {/each}
+                {/await}
+                
+                <div class="addcard">
+                    <sl-button variant="default" size="large" circle>
+                        <i class="fa-solid fa-plus-large"></i>
+                    </sl-button>
                 </div>
-            </sl-card>
-            {/each}
-            
-            <sl-card class="card addcard fullwidth">
-                <div>
-                    <i class="fa-solid fa-circle-plus"></i>
-                </div>
-            </sl-card>
-            {/if}
+                
+            </div>
             {/await}
             
+            {/each}
         </div>
-        {/await}
-        
-        {/each}
     </div>
-</div>
 </div>
 
 {/await}
@@ -192,24 +203,21 @@
     }
     .boardscroll {
         overflow-x:scroll;
+        
     }
     .board {
         margin: 0 calc((100% - 80rem)/2) 3rem;
         padding: 20px;
         min-height: 70vh;
     }
+    .row {
+        /* 
+        background: rgb(255,255,255);
+        background: linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(248,248,248,1) 13%); 
+        */
+    }
     .column {
         min-width: 30rem;
-    }
-    .card {
-        border-width: 3px 1px 1px;
-        box-shadow: var(--sl-shadow-large);
-        margin-bottom: 1rem;
-    }
-    .card [slot='footer'] {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
     }
     .addcard {
         width: 100%;
